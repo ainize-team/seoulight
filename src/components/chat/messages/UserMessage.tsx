a8234e1d96ced641043ecbfc1aba7fc27a1f6f9b
ainize-dev <ainize-dev@comcom.ai>
2025-04-06 07:24:33 +0900

    set: gap

--- src/components/chat/messages/UserMessage.tsx
+++ src/components/chat/messages/UserMessage.tsx
@@ -4,7 +4,7 @@ import { UserMessageType } from "@/types/MessageType";
 
 export default function UserMessage({ message }: { message: UserMessageType }) {
   return (
-    <div className="flex w-full">
+    <div className="my-5 flex w-full">
       <div className="flex items-center space-x-2">
         <div className="flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full bg-[#c7bba7] text-sm font-bold text-white">
           나
